38b8a1f9f29bddf6db574637f939ec87e68008c5
ronafa <ronafa@post.bgu.ac.il>
2022-03-22 21:38:21 +0200

    add gravity

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 let gElGame, gElPipe, gElLid, gElBird, gElScore, gElModal
 let isGameOver = false
+let isOnMove = false
 
-initGame()
+// initGame()
 
 function initGame() {
   getElements()
   setLids()
+  setEventListeners()
+  setGravity()
   setAnimation()
 }
 
@@ -27,19 +30,82 @@ function setAnimation() {
 }
 
 function setLids() {
-    console.log(gElLid);
-    gElLid.addEventListener('animationiteration', () => {
-        const topHeight = 0.6 * window.innerHeight
-        const bottomHeight = 0.95 * window.innerHeight
-        const randTop = getRandomIntInclusive(topHeight, bottomHeight)
-        console.log('randTop' , randTop);
-        gElLid.style.top = `-${randTop}px`
-    })
+  gElLid.addEventListener('animationiteration', () => {
+    const topHeight = 0.6 * window.innerHeight
+    const bottomHeight = 0.95 * window.innerHeight
+    const randTop = getRandomIntInclusive(topHeight, bottomHeight)
+    gElLid.style.top = `-${randTop}px`
+  })
 }
 
 function setEventListeners() {
-    window.addEventListener('resize', () => {
-        if(!isGameOver) return
-        setAnimation()
-    })
+  window.addEventListener('resize', () => {
+    if (isGameOver) return
+    setAnimation()
+  })
+
+  document.body.addEventListener('click', () => {
+    if (isGameOver) return
+    birdMove()
+  })
+
+  document.onkeypress = (ev) => {
+    ev = ev || window.event
+
+    if (ev.keyCode === 32) {
+      if (isGameOver) return
+      birdMove()
+    }
+  }
+}
+
+function birdMove() {
+  isOnMove = true
+  var moveCount = 0
+
+  const moveInterval = setInterval(() => {
+    changeGameState({ direction: 'up', diff: -3 })
+    if (moveCount > 20) {
+      clearInterval(moveInterval)
+      isOnMove = false
+      moveCount = 0
+    }
+    moveCount++
+  }, 10)
+}
+
+function changeGameState({ direction, diff }) {
+  handleAnimation(direction)
+  handlePosition(diff)
+}
+
+function handleAnimation(direction) {
+  if (direction === 'up') {
+    gElBird.classList.add('move-up')
+    gElBird.classList.remove('move-down')
+  } else if (direction === 'down') {
+    gElBird.classList.remove('move-up')
+    gElBird.classList.add('move-down')
+  }
+}
+
+function handlePosition(diff) {
+    const currTop = parseInt(getComputedStyle(gElBird).top)
+    const newTop = currTop + diff
+
+    if(newTop < 0) return
+    if(newTop > window.innerHeight) return gameOver()
+
+    gElBird.style.top = `${newTop}px`
+}
+
+function setGravity() {
+    setInterval(() => {
+        if(isOnMove) return
+        changeGameState({direction: 'down', diff: 5})
+    },20)
+}
+
+function gameOver() {
+
 }
